d1655b03aab47a179cc934d0cbf44e8f4e10a416
kshpthk <st179561@stud.uni-stuttgart.de>
2025-08-14 15:18:19 +0200

    updated testsuite to just use the MINDFul testsuite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,19 @@
+using Pkg
+using MINDFul
+
+# Get the path to MINDFul package
+mindful_path = dirname(dirname(pathof(MINDFul)))
+testsuite_path = joinpath(mindful_path, "test", "testsuite")
+
 include("initialize.jl")
-include("testsuite/utils.jl")
-include("testsuite/physicaltest.jl")
-include("testsuite/basicintenttest.jl")
-include("testsuite/opticalconstraintssingledomain.jl")
-include("testsuite/multidomain.jl")
-include("testsuite/failingintime.jl")
-include("testsuite/grooming.jl")
-include("testsuite/groomingonfail.jl")
-include("testsuite/interface.jl")
+include(joinpath(testsuite_path, "utils.jl"))
+include(joinpath(testsuite_path, "physicaltest.jl"))
+include(joinpath(testsuite_path, "basicintenttest.jl"))
+include(joinpath(testsuite_path, "opticalconstraintssingledomain.jl"))
+include(joinpath(testsuite_path, "multidomain.jl"))
+include(joinpath(testsuite_path, "failingintime.jl"))
+include(joinpath(testsuite_path, "grooming.jl"))
+include(joinpath(testsuite_path, "groomingonfail.jl"))
+include(joinpath(testsuite_path, "interface.jl"))
 
 nothing
